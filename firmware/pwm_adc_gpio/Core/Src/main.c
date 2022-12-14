a76a7d629d8cbf7802e80ffdf69a5cca2f1a0b3d
CrissoGB <62680224+CrissoGB@users.noreply.github.com>
2022-12-14 15:40:53 -0300

    usart1 a uart4

--- firmware/pwm_adc_gpio/Core/Src/main.c
+++ firmware/pwm_adc_gpio/Core/Src/main.c
@@ -119,8 +119,8 @@ int main(void)
   MX_USART2_UART_Init();
   MX_ADC1_Init();
   MX_TIM1_Init();
-  MX_USART1_UART_Init();
   MX_TIM2_Init();
+  MX_UART4_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
